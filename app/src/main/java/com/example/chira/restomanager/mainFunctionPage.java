4e85753915f0c386f6d418f8904ad0626a0c85d7
Chirag-Rathi <chiragrathi9101@gmail.com>
2018-09-24 20:43:09 +0530

    Basic GUI

--- app/src/main/java/com/example/chira/restomanager/mainFunctionPage.java
+++ app/src/main/java/com/example/chira/restomanager/mainFunctionPage.java
@@ -2,15 +2,12 @@ package com.example.chira.restomanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button button;
+public class mainFunctionPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_function_page);
     }
 }
